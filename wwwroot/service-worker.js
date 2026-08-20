a4a275c07fdfc5275294b4c79e798007fe0c2534
Mustafa Arikan <mustafaarikanmail.com>
2026-08-20 22:31:57 +0200

    Benachrichtigung sende Fehler gelöst. service-worker Datei hinzugefügt für PWA Config.

---
+++ wwwroot/service-worker.js
@@ -0,0 +1,17 @@
+﻿// wwwroot/service-worker.js
+
+self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
+    return self.clients.claim();
+});
+
+// A fetch listener is required by some mobile browsers (like Chrome) 
+// to recognize the app as a valid PWA, even if it does nothing.
+self.addEventListener('fetch', event => {
+    return;
+});
